952fe3a3dcee97ad8656efc918a3e2e036b5f188
Zenghui Shi <zshi@redhat.com>
2018-01-30 11:35:07 +0800

    fix overcloud deploy environment file path

--- environments/rdo_newton_dpdk_uc_master_oc_newton/overcloud_deploy_scale.sh
+++ environments/rdo_newton_dpdk_uc_master_oc_newton/overcloud_deploy_scale.sh
@@ -7,6 +7,6 @@ openstack overcloud deploy --templates /home/stack/tripleo-heat-templates-newton
 -e /home/stack/tripleo-heat-templates-newton/environments/network-isolation.yaml \
 -e /home/stack/tripleo-heat-templates-newton/environments/net-single-nic-with-vlans.yaml \
 -e /home/stack/tripleo-heat-templates-newton/environments/neutron-ovs-dpdk.yaml \
--e /home/stack/rdo_newton_dpdk/network-environment.yaml \
+-e /home/stack/rdo_newton_dpdk_uc_master_oc_newton/network-environment.yaml \
 --ntp-server pool.ntp.org \
 --log-file overcloud_install.log
