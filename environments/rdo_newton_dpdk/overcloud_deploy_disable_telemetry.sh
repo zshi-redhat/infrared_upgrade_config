79a00d943bad2b7e5d0af2786250626d83e0702b
Zenghui Shi <zshi@redhat.com>
2018-03-15 15:40:49 +0800

    add disable telemetry env template

---
+++ environments/rdo_newton_dpdk/overcloud_deploy_disable_telemetry.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+openstack overcloud deploy --templates \
+--control-scale 3 --compute-scale 2 --ceph-storage-scale 0 \
+--control-flavor controller --compute-flavor compute \
+-e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml  \
+-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
+-e /home/stack/rdo_newton_dpdk/network-environment.yaml \
+-e /home/stack/rdo_newton_dpdk/disable_telemetry.yaml \
+--ntp-server 10.5.26.10 \
+--log-file overcloud_install.log
