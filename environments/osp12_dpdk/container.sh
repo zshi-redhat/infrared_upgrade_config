08bd41d33f081cf98a9e6ebbcce0931fd434067c
Zenghui Shi <zshi@redhat.com>
2018-04-13 11:53:34 +0800

    Change oc container prepare and deploy cmds

--- environments/osp12_dpdk/container.sh
+++ environments/osp12_dpdk/container.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 openstack overcloud container image prepare   \
-  --namespace 10.60.19.51:5000/rhosp12  \
+  --namespace docker-registry.engineering.redhat.com/rhosp12  \
   --prefix "openstack-" \
-  --suffix "docker" \
   --tag latest \
   --env-file docker_registry.yaml \
   --service-environment-file /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml
 
+#openstack overcloud container image prepare   \
+#  --namespace docker-registry.engineering.redhat.com/rhosp12  \
+#  --prefix "openstack-" \
+#  --suffix "docker" \
+#  --tag latest \
+#  --env-file docker_registry.yaml \
+#  --service-environment-file /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml
+
 #sed -i 's/docker:/-docker:/g' docker_registry.yaml
-echo "  DockerInsecureRegistryAddress: 10.60.19.51:5000" >> docker_registry.yaml
-echo "  DockerNamespace: 10.60.19.51:5000/rhosp12" >> docker_registry.yaml
-echo "  DockerNamespaceIsRegistry: true" >> docker_registry.yaml
+#echo "  DockerInsecureRegistryAddress: 10.60.19.51:5000" >> docker_registry.yaml
+#echo "  DockerNamespace: 10.60.19.51:5000/rhosp12" >> docker_registry.yaml
+#echo "  DockerNamespaceIsRegistry: true" >> docker_registry.yaml
--- environments/osp12_dpdk/overcloud_deploy.sh
+++ environments/osp12_dpdk/overcloud_deploy.sh
@@ -3,15 +3,13 @@
 openstack overcloud deploy \
     --templates \
     -r /home/stack/osp12_dpdk/roles_data.yaml \
-    -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml \
     --timeout 90 \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
-    -e registry.yaml \
     -e /home/stack/osp12_dpdk/network-environment.yaml \
-    -e docker_registry.yaml
-    --ntp-server pool.ntp.org \
+    -e /home/stack/docker_registry.yaml
+    --ntp-server 10.5.26.10 \
     --log-file overcloud_install.log
